3541496f11d7246f55f65cccee1a57f2dc52d4a4
vadimlasevich <lasevichv@mail.ru>
2023-02-25 02:49:32 +0300

    Update 2023-02-24T23:49:29.599Z

--- gulp/tasks/deploy.js
+++
@@ -1,5 +0,0 @@
-import deploySite from "gulp-gh-pages";
-
-export const deploy = () => {
-  return app.gulp.src(`${app.path.buildFolder}/**/*.*`, {}).pipe(deploySite());
-};
--- src/js/modules/maskInput.js
+++
@@ -1,7 +0,0 @@
-const maskInput = () => {
-  let input = document.querySelector('input[type="tel"]');
-  let im = new Inputmask('+7 (999) 999-99-99');
-  im.mask(input);
-};
-
-export default maskInput;
